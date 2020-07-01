f100b08f9c74fd58d3a62337e5f5c02f91eae3e3
q7800067 <280014580@qq.com>
2020-07-01 17:20:15 +0800

    1

--- 基于多态的企业职工系统/worker.h
+++ 基于多态的企业职工系统/worker.h
@@ -7,12 +7,10 @@ using namespace std;
 class worker
 {
 public:
-
 	//显示个人信息
 	virtual void showinfo() = 0;
 	//获取岗位名称
 	virtual string getdeptname() = 0;
-
 	int m_id; //职工编号
 	string m_name; //职工姓名
 	int m_deptid; //职工所在部门名称编号
